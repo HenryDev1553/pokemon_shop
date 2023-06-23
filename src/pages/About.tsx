fd0cd39143371587a2eaf6e9e8376dc2924a03d1
HenryDev1553 <HenryDev1553>
2023-06-23 16:40:27 +0700

    first

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/kishan.jpeg";
-import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
+import avatarImage from "../assets/henry.jpg";
+import { FaYoutube, FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 function About() {
   return (
     <div className="profile">
       <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
+      <h1 className="profile-text">Hi I am Henry Dev</h1>
       <h2 className="profile-text">The creator of this awesome pokedex</h2>
-      <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
-      </h4>
+
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="https://github.com/henrydev1553">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
-          <FaYoutube />
-        </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
-          <FaLinkedin />
+        <a href="https://www.facebook.com/namngn15203">
+          <FaFacebook />
         </a>
       </div>
     </div>
